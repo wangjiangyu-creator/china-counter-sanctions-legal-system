e40b256feff9db0f893c87fdd7d3a79e8b497af1
wangjiangyu-creator <wangjiangyu@gmail.com>
2026-05-14 11:52:41 +0800

    Add article dataset chunk 1

---
+++ src/data/articles.part1.js
@@ -0,0 +1,238 @@
+export const articlesPart1 = [
+  {
+    "id": "poon-2023-context",
+    "title": "Extraterritoriality, Sanctions, and the Fight for Market: Putting China's Anti-Foreign Sanctions Law into Context",
+    "authors": [
+      "Benjamin K. N. Poon"
+    ],
+    "publisher": "SSRN",
+    "year": 2023,
+    "language": "en",
+    "sourceKinds": [
+      "scholar"
+    ],
+    "jurisdictions": [
+      "China",
+      "United States"
+    ],
+    "relatedLawIds": [
+      "cn-afsl-2021",
+      "cn-foreign-relations-law-2023",
+      "cn-extraterritorial-jurisdiction-2026"
+    ],
+    "abstract": "\u4ece\u6bd4\u8f83\u6cd5\u548c\u56fd\u9645\u7ecf\u6d4e\u6cd5\u89d2\u5ea6\u8ba8\u8bba\u4e2d\u56fd\u300a\u53cd\u5916\u56fd\u5236\u88c1\u6cd5\u300b\u7684\u5236\u5ea6\u80cc\u666f\uff0c\u5e76\u5c06\u5176\u653e\u56de\u4e2d\u7f8e\u5236\u88c1\u7ade\u4e89\u4e0e\u57df\u5916\u7ba1\u8f96\u5386\u53f2\u4e2d\u89c2\u5bdf\u3002",
+    "accessMode": "summary",
+    "sourceUrl": "https://papers.ssrn.com/sol3/papers.cfm?abstract_id=4444856"
+  },
+  {
+    "id": "svetlicinii-2022-eu-lessons",
+    "title": "China's Defense Against Secondary Sanctions: Lessons from the EU Blocking Statute",
+    "authors": [
+      "Alexandr Svetlicinii"
+    ],
+    "publisher": "SSRN / Journal of International Trade Law and Policy",
+    "year": 2022,
+    "language": "en",
+    "sourceKinds": [
+      "scholar"
+    ],
+    "jurisdictions": [
+      "China",
+      "European Union"
+    ],
+    "relatedLawIds": [
+      "cn-afsl-2021",
+      "cn-blocking-rules-2021",
+      "eu-blocking-statute",
+      "uk-ptia-1980"
+    ],
+    "abstract": "\u805a\u7126\u4e2d\u56fd\u53cd\u5236\u6b21\u7ea7\u5236\u88c1\u5236\u5ea6\u4e0e\u6b27\u76df\u963b\u65ad\u6cd5\u4e4b\u95f4\u7684\u6bd4\u8f83\uff0c\u91cd\u70b9\u8ba8\u8bba\u79c1\u4eba\u4e3b\u4f53\u9075\u5b88\u5916\u56fd\u63aa\u65bd\u3001\u7981\u4ee4\u673a\u5236\u4e0e\u6551\u6d4e\u8def\u5f84\u3002",
+    "accessMode": "summary",
+    "sourceUrl": "https://papers.ssrn.com/sol3/papers.cfm?abstract_id=4262523"
+  },
+  {
+    "id": "cjcl-2022-emerging-unilateral-sanctions",
+    "title": "China's Emerging Unilateral Sanctions: Heading towards the Rule of Law?",
+    "authors": [
+      "Linjing Zhang"
+    ],
+    "publisher": "The Chinese Journal of Comparative Law",
+    "year": 2022,
+    "language": "en",
+    "sourceKinds": [
+      "scholar"
+    ],
+    "jurisdictions": [
+      "China"
+    ],
+    "relatedLawIds": [
+      "cn-afsl-2021",
+      "cn-foreign-relations-law-2023",
+      "cn-blocking-rules-2021"
+    ],
+    "abstract": "\u8ba8\u8bba\u4e2d\u56fd\u5355\u8fb9\u5236\u88c1\u5236\u5ea6\u7684\u6cd5\u6cbb\u5316\u8def\u5f84\uff0c\u5173\u6ce8\u300a\u53cd\u5916\u56fd\u5236\u88c1\u6cd5\u300b\u4e0e\u5176\u4ed6\u6d89\u5916\u6cd5\u5f8b\u4e4b\u95f4\u7684\u8854\u63a5\u95ee\u9898\u3002",
+    "accessMode": "summary",
+    "sourceUrl": "https://academic.oup.com/cjcl/article/9/3/359/6550756"
+  },
+  {
+    "id": "cms-2025-afsl-implementation",
+    "title": "New Rules on the Implementation of China's Anti-Foreign Sanctions Law",
+    "authors": [
+      "CMS China Team"
+    ],
+    "publisher": "CMS",
+    "year": 2025,
+    "language": "en",
+    "sourceKinds": [
+      "law-firm"
+    ],
+    "jurisdictions": [
+      "China"
+    ],
+    "relatedLawIds": [
+      "cn-afsl-implementation-2025",
+      "cn-afsl-2021"
+    ],
+    "abstract": "\u9762\u5411\u4f01\u4e1a\u5408\u89c4\u5b9e\u8df5\uff0c\u6982\u62ec2025\u5e74\u5b9e\u65bd\u89c4\u5b9a\u5982\u4f55\u7ec6\u5316\u53cd\u5236\u6e05\u5355\u3001\u53cd\u5236\u63aa\u65bd\u4e0e\u6267\u6cd5\u534f\u8c03\u673a\u5236\u3002",
+    "accessMode": "summary",
+    "sourceUrl": "https://cms.law/en/chn/news-information/new-rules-on-the-implementation-of-china-s-anti-foreign-sanctions-law"
+  },
+  {
+    "id": "dlapiper-2021-blocking-rules",
+    "title": "China's Blocking Rules: New Trade-Compliance Dilemmas for Third-Country Companies",
+    "authors": [
+      "DLA Piper"
+    ],
+    "publisher": "DLA Piper",
+    "year": 2021,
+    "language": "en",
+    "sourceKinds": [
+      "law-firm"
+    ],
+    "jurisdictions": [
+      "China"
+    ],
+    "relatedLawIds": [
+      "cn-blocking-rules-2021"
+    ],
+    "abstract": "\u4ece\u8de8\u5883\u8d38\u6613\u5408\u89c4\u7684\u89d2\u5ea6\u5206\u6790\u5546\u52a1\u90e8\u963b\u65ad\u89c4\u5219\uff0c\u91cd\u70b9\u8ba8\u8bba\u5176\u9002\u7528\u95e8\u69db\u300130\u65e5\u62a5\u544a\u4e49\u52a1\u3001\u7981\u4ee4\u673a\u5236\u548c\u5bf9\u8de8\u56fd\u516c\u53f8\u7684\u51b2\u7a81\u6cd5\u538b\u529b\u3002",
+    "accessMode": "summary",
+    "sourceUrl": "https://www.dlapiper.com/insights/publications/2021/01/chinas-blocking-rules-new-trade-compliance-dilemmas-for-third-country-companies"
+  },
+  {
+    "id": "jonesday-2025-afsl-enforcement",
+    "title": "China Boosts Enforcement of Anti-Foreign Sanctions Law",
+    "authors": [
+      "Jones Day"
+    ],
+    "publisher": "Jones Day",
+    "year": 2025,
+    "language": "en",
+    "sourceKinds": [
+      "law-firm"
+    ],
+    "jurisdictions": [
+      "China"
+    ],
+    "relatedLawIds": [
+      "cn-afsl-2021",
+      "cn-afsl-implementation-2025",
+      "cn-extraterritorial-jurisdiction-2026"
+    ],
+    "abstract": "\u4ece\u4f01\u4e1a\u8fd0\u8425\u89c6\u89d2\u68b3\u7406\u4e2d\u56fd\u57282025\u5e74\u540e\u5f3a\u5316\u6267\u884c\u300a\u53cd\u5916\u56fd\u5236\u88c1\u6cd5\u300b\u7684\u4e3b\u8981\u53d8\u5316\uff0c\u5e76\u63d0\u793a\u4ea4\u6613\u4e0e\u4f9b\u5e94\u94fe\u98ce\u9669\u3002",
+    "accessMode": "summary",
+    "sourceUrl": "https://www.jonesday.com/de/insights/2025/04/china-boosts-enforcement-of-antiforeign-sanctions-law"
+  },
+  {
+    "id": "mayerbrown-2020-uel",
+    "title": "China's 'Unreliable Entities List': With the Provisions Now in Effect, What Do Global Companies Need to Know?",
+    "authors": [
+      "Mayer Brown"
+    ],
+    "publisher": "Mayer Brown",
+    "year": 2020,
+    "language": "en",
+    "sourceKinds": [
+      "law-firm"
+    ],
+    "jurisdictions": [
+      "China"
+    ],
+    "relatedLawIds": [
+      "cn-unreliable-entity-list-2020"
+    ],
+    "abstract": "\u56f4\u7ed5\u4e0d\u53ef\u9760\u5b9e\u4f53\u6e05\u5355\u89c4\u5b9a\u7684\u6784\u6210\u8981\u4ef6\u3001\u6f5c\u5728\u5236\u88c1\u63aa\u65bd\u548c\u8de8\u5883\u51b2\u7a81\u6cd5\u98ce\u9669\uff0c\u63d0\u4f9b\u5b9e\u52a1\u5bfc\u5411\u5206\u6790\u3002",
+    "accessMode": "summary",
+    "sourceUrl": "https://www.mayerbrown.com/en/insights/events/2020/09/chinas-unreliable-entities-list-with-the-provisions-now-in-effect-what-do-global-companies-need-to-know"
+  },
+  {
+    "id": "dlapiper-2020-uel",
+    "title": "China's Unreliable Entity List Provisions: Fighting Fire with Fire?",
+    "authors": [
+      "DLA Piper"
+    ],
+    "publisher": "DLA Piper",
+    "year": 2020,
+    "language": "en",
+    "sourceKinds": [
+      "law-firm"
+    ],
+    "jurisdictions": [
+      "China"
+    ],
+    "relatedLawIds": [
+      "cn-unreliable-entity-list-2020",
+      "cn-export-control-law-2020"
+    ],
+    "abstract": "\u4ece\u5236\u88c1\u3001\u51fa\u53e3\u7ba1\u5236\u4e0e\u56fd\u5bb6\u5b89\u5168\u653f\u7b56\u8054\u52a8\u7684\u89d2\u5ea6\u89e3\u6790\u4e0d\u53ef\u9760\u5b9e\u4f53\u6e05\u5355\u5236\u5ea6\u7684\u8bbe\u8ba1\u548c\u5bf9\u8de8\u56fd\u516c\u53f8\u7684\u5f71\u54cd\u3002",
+    "accessMode": "summary",
+    "sourceUrl": "https://www.dlapiper.com/en-us/insights/publications/2020/09/china-unreliable-entity-list-provisions-fighting-fire-with-fire"
+  },
+  {
+    "id": "chinalawinsight-2020-export-control",
+    "title": "New Export Control Law: 5 Issues Remain to Be Clarified",
+    "authors": [
+      "King & Wood Mallesons China Law Insight"
+    ],
+    "publisher": "China Law Insight",
+    "year": 2020,
+    "language": "en",
+    "sourceKinds": [
+      "law-firm",
+      "practitioner"
+    ],
+    "jurisdictions": [
+      "China"
+    ],
+    "relatedLawIds": [
+      "cn-export-control-law-2020"
+    ],
+    "abstract": "\u56f4\u7ed5\u4e2d\u56fd\u51fa\u53e3\u7ba1\u5236\u6cd5\u751f\u6548\u521d\u671f\u7684\u5b9a\u4e49\u8fb9\u754c\u3001\u6e05\u5355\u7ba1\u7406\u3001\u6267\u6cd5\u8def\u5f84\u548c\u5bf9\u5916\u53cd\u5236\u6761\u6b3e\u63d0\u51fa\u5b9e\u52a1\u95ee\u9898\u6e05\u5355\u3002",
+    "accessMode": "summary",
+    "sourceUrl": "https://www.chinalawinsight.com/2020/11/articles/export/new-export-control-law-5-issues-remains-to-be-clarified/"
+  },
+  {
+    "id": "shang-2024-export-control",
+    "title": "Mapping Export Control Extraterritoriality in the U.S.-China Technology Decoupling",
+    "authors": [
+      "Carrie Shu Shang"
+    ],
+    "publisher": "SSRN",
+    "year": 2024,
+    "language": "en",
+    "sourceKinds": [
+      "scholar"
+    ],
+    "jurisdictions": [
+      "China",
+      "United States"
+    ],
+    "relatedLawIds": [
+      "cn-export-control-law-2020",
+      "cn-foreign-trade-law-2025"
+    ],
+    "abstract": "\u4ece\u5b66\u7406\u5c42\u9762\u8ba8\u8bba\u51fa\u53e3\u7ba1\u5236\u57df\u5916\u6027\u4e0e\u6280\u672f\u8131\u94a9\uff0c\u5e76\u4e3a\u7406\u89e3\u4e2d\u56fd\u51fa\u53e3\u7ba1\u5236\u6cd5\u548c\u5916\u8d38\u6cd5\u4e2d\u7684\u5bf9\u7b49\u56de\u5e94\u63d0\u4f9b\u80cc\u666f\u3002",
+    "accessMode": "summary",
+    "sourceUrl": "https://papers.ssrn.com/sol3/papers.cfm?abstract_id=4701850"
+  }
+];
